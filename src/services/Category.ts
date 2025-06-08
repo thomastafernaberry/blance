601e8a92f52d28704a1c4eb6f27634f20785c2a8
Thomas Tafernaberry <thomastafernaberry@gmail.com>
2025-06-07 23:18:26 -0300

    add new components. add category and product routing. add Category.ts service

---
+++ src/services/Category.ts
@@ -0,0 +1,24 @@
+import Strapi from './Strapi.ts';
+
+export default class Category extends Strapi {
+
+	private collectionName: string;
+	private categoryCollection: any;
+	
+	private constructor() {
+		super();
+		this.collectionName = 'categories';
+	}
+	
+	static async init() : Promise<Category> {
+		const instance = new Category();
+		instance.categoryCollection = await instance.strapiClient.collection(instance.collectionName);
+		return instance;
+	}
+
+	async getAllNames(name: string) : Promise<Response> {
+		const collection = await this.categoryCollection.find();
+		return collection.data.map(c => c.name);
+	}
+
+}
