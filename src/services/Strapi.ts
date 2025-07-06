984b48d861605f51663942bc0fda2654ed01d31e
Thomas Tafernaberry <thomastafernaberry@gmail.com>
2025-07-06 03:39:13 -0300

    strapi service now use enviromental variables.

--- src/services/Strapi.ts
+++ src/services/Strapi.ts
@@ -2,6 +2,9 @@ import type { ProductData } from './Product.ts';
 import type { CategoryData } from './Category.ts';
 import { strapi } from '@strapi/client';
 
+const strapiURL = import.meta.env.PUBLIC_STRAPI_URL;
+const strapiToken = import.meta.env.STRAPI_TOKEN;
+
 export type StrapiResponse = {
   readonly data: ProductData | ProductData[] | CategoryData[];
   readonly meta: any;
@@ -14,8 +17,8 @@ export default class Strapi {
 	public strapiClient: any;
 	
 	constructor() {
-		this.strapiURL = 'http://localhost:1337/api';
-		this.strapiToken = '9dbef15e6c8c06dd0b7596cb18f47a36617f5077bd62bdfc2a9efb86a5e6b5bbf39906659e1232ad0d46559c1eddc2abfb1c6ce8c1453e0f48632f3faee38309d48af53a5c684a793d67c2ba00a18c03eaa80a415b7561c427fdf456bc31c1060d0f836862e1d528298fbaf3b0f4d6d6fa3a49f11a418d5a4c3bfb90f92a85c4';
+		this.strapiURL = strapiURL;
+		this.strapiToken = strapiToken;
 		this.strapiClient = strapi({ 
 			baseURL: this.strapiURL, 
 			auth: this.strapiToken 
