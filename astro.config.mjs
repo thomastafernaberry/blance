ecaeaf138f678a89aaae5472a53a926df35ef9ca
Thomas Tafernaberry <thomastafernaberry@gmail.com>
2026-07-13 16:22:08 -0300

    update Astro to 5.10.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,9 @@ import node from "@astrojs/node";
 // https://astro.build/config
 export default defineConfig({
   vite: { plugins: [tailwindcss()], },
+	image: {
+		remotepatterns: [{ protocol: 'https' }],
+	},
   adapter: node({
     mode: 'standalone',
   }),
